f413e1c40cfcc704bcfd6448804d4a1340770c82
Vishalendu Pandey <vishalendu@gmail.com>
2022-12-29 14:23:42 -0600

    Add files via upload

--- GatlingTut1.java
+++ GatlingTut1.java
@@ -17,17 +17,15 @@ public class GatlingTut1 extends Simulation{
     private static String jwt="";
 
     private HttpProtocolBuilder httpProtocol = http
-            .baseUrl("http://localhost:3000")
+            .baseUrl("http://localhost:3001")
             .contentTypeHeader("application/json")
-            .acceptHeader("application/json")
-            .header("Authenticate","Bearer #{token}");
-
+            .acceptHeader("application/json");
 
     private ChainBuilder setToken = exec(
-            session -> session.set("token", GatlingTut1.jwt));
+            session -> session.set("token", GatlingTut1.jwt)
+            );
     private ChainBuilder getAuth =
-            exec(setToken)
-            .exec(http("get auth token")
+            exec(http("get auth token")
             .post("/authorize")
             .body(StringBody("{\"username\":\"admin\",\"password\":\"password\"}")
             )
@@ -40,45 +38,39 @@ public class GatlingTut1 extends Simulation{
                 return session;
                     })
             ;
-
-
+    
     private ChainBuilder order =
             exec(setToken)
+            .exec(session -> {
+                System.out.println("****** token = "+session.get("token"));
+                return session;
+            })
             .exec(http("createOrder")
             .post("/order")
+            .header("Authenticate","Bearer #{token}")
             .body(StringBody(session -> getPayload()))
             .check(status().saveAs("status"))
             .check(status().is(200))
             );
     private ScenarioBuilder scn = scenario("Get Auth")
-            .during(Duration.ofSeconds(120)).on(
+            .during(120).on(
              exec(getAuth)
-            .pause(Duration.ofSeconds(60)
-            ));
+            .pause(60)
+            );
 
     private ScenarioBuilder scn1 = scenario("Create Order")
-            .during(Duration.ofSeconds(120)).on(
-            pause(Duration.ofSeconds(5)) // wait for auth request to complete
-            .exec(order)
-            .pause(Duration.ofSeconds(10)
-            ));
-
+            .during(120).on(
+                exec(order)
+                .pause(10)
+            );
 
 
     {
-
         setUp(
-                scn.injectOpen(
-                        atOnceUsers(1)
-                ),
-                scn1.injectOpen(
-                        atOnceUsers(1)
-                )
+                scn.injectOpen(atOnceUsers(1)),
+                scn1.injectOpen(nothingFor(5), // let the auth return a token
+                                atOnceUsers(1))
         ).protocols(httpProtocol);
-
-
-
-
     }
 
     public String getPayload(){
